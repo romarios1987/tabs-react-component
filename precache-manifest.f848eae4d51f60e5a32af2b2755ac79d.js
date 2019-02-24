c17c2c50e6254f9c7d606ed52f47fbaea4fd0dda
romarios1987 <romios047@gmail.com>
2019-02-24 22:39:17 +0300

    Updates

---
+++ precache-manifest.f848eae4d51f60e5a32af2b2755ac79d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38c61d1bcf8f9144a997",
+    "url": "/tabs-react-component/static/js/runtime~main.38c61d1b.js"
+  },
+  {
+    "revision": "7c2b1810a6e9d7d1618d",
+    "url": "/tabs-react-component/static/js/main.7c2b1810.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/tabs-react-component/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "7c2b1810a6e9d7d1618d",
+    "url": "/tabs-react-component/static/css/main.c7c70a0b.chunk.css"
+  },
+  {
+    "revision": "21e01eda0da6d1285888b996eb5865ce",
+    "url": "/tabs-react-component/index.html"
+  }
+];
