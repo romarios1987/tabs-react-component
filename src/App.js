d7cd7798b73c7216cd2ce3c986d06d6c77d5000b
romarios1987 <romios047@gmail.com>
2019-02-24 22:24:09 +0300

    React installation

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+          <div className="app">
+            <div className="container">
+              <h1>Tabs Component</h1>
+              <div className="wrapper-tabs">
+
+              </div>
+            </div>
+          </div>
+    );
+  }
+}
+
+export default App;
